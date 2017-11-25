a48f262e927fbdec4e1df3cd55d8173c90012dd3
ollestenborg <olle.stenborg@gmail.com>
2017-11-25 14:55:09 +0100

    Update criteria.js

--- criteria.js
+++ criteria.js
@@ -12,7 +12,7 @@ const datalist=document.createElement("datalist")
 options.map((item)=>{
 const op=document.createElement("option")
 op.innerText=item
-select.appendChild(op)
+datalist.appendChild(op)
 })
 return datalist
 }
